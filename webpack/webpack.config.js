48fe325a206289b3bfefe753182d6260eddc34c4
Guilherme-Castello-Clear <guilherme.castello.clear@gmail.com>
2023-04-20 10:55:53 -0300

    Feat: Using Webpack-Server and Webpack-Loader

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,13 +1,28 @@
+const modoDev = process.env.NODE_ENV !== 'production'
 const webpack = require('webpack')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
 
 module.exports = {
-
-    mode: 'development',
+    mode: modoDev ? 'development' : 'production',
     entry: './src/principal.js',
     output: {
         filename: 'principal.js',
         path: __dirname + '/public'
+    },
+    devServer: {
+        contentBase: "./public",
+        port: 9000
+    },
+    optimization: {
+        minimizer: [
+            new UglifyJsPlugin({
+                cache: true,
+                parallel: true
+            }),
+            new OptimizeCSSAssetsPlugin({})
+        ]
     },
     plugins: [
         new MiniCssExtractPlugin({
@@ -15,16 +30,18 @@ module.exports = {
         })
     ],
     module: {
-        rules: [{
+        rules: [
+            {
             test: /\.s?[ac]ss$/,
             use: [
                 MiniCssExtractPlugin.loader,
-                'css-loader',
-                'sass-loader'
+                'css-loader', 
+                'sass-loader',
             ]
-        }, {
-            test: /\.(png|svg|jpg|gif)$/,
-            use: ['file-loader']
-        }]
-    }
+            }, 
+            {
+                test: /\.(png|svg|jpeg|jpg|gif)$/,
+                use: ['file-loader']
+            }]
+        }
 }
